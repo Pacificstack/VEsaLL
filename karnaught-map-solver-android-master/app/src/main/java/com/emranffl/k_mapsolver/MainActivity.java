a1062e807b11393f17876f84552b560c381ba1e5
Pacificstack <papu225544@gmail.com>
2021-12-11 23:19:53 +0530

    direct commit

---
+++ karnaught-map-solver-android-master/app/src/main/java/com/emranffl/k_mapsolver/MainActivity.java
@@ -0,0 +1,85 @@
+package com.emranffl.k_mapsolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RadioButton ab;
+    private RadioButton abc;
+    private RadioButton abcd;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Karnaugh Map");//change name
+
+
+
+
+        ab = findViewById(R.id.ab);
+        abc = findViewById(R.id.abc);
+        abcd = findViewById(R.id.abcd);
+        imageView = findViewById(R.id.imageView);
+
+
+       /* imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com")));
+            }
+        });*/
+
+        ab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (abc.isChecked()) {
+                    abc.setChecked(false);
+                } else if (abcd.isChecked()) {
+                    abcd.setChecked(false);
+                }
+
+                ab.setChecked(false);
+                startActivity(new Intent(getApplicationContext(), TwoVar_Layout.class));
+            }
+        });
+
+        abc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (abcd.isChecked()) {
+                    abcd.setChecked(false);
+                } else if (ab.isChecked()) {
+                    ab.setChecked(false);
+                }
+
+                abc.setChecked(false);
+                startActivity(new Intent(getApplicationContext(), ThreeVar_Layout.class));
+            }
+        });
+
+        abcd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ab.isChecked()) {
+                    ab.setChecked(false);
+                } else if (abc.isChecked()) {
+                    abc.setChecked(false);
+                }
+
+                abcd.setChecked(false);
+                startActivity(new Intent(getApplicationContext(), FourVar_Layout.class));
+            }
+        });
+
+
+    }
+}
